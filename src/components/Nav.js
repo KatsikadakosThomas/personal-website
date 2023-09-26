01773fc24a9fa5c02b42bac33544684c29daceb5
KatsikadakosThomas <tkatsika@gmail.com>
2023-09-26 10:43:27 +0300

    remove dark mode

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,15 +7,15 @@ export default function Nav(){
         <nav className="absolute left-0 top-0 z-[200] h-[50px] sm:h-20 w-full glass shadow font-bold flex flex-row justify-end sm:justify-center items-center">
             <div className="m-2 hidden sm:block">
                 <span className="text-cyan-500">01. </span>
-                <Link href="/">HOME</Link>
+                <Link className="text-white" href="/">HOME</Link>
                 </div>
             <div className="m-2  hidden sm:block">
                 <span className="text-cyan-500">02. </span>
-                <Link href="/resume">RESUME</Link>
+                <Link className="text-white" href="/resume">RESUME</Link>
                 </div>
             <div className="m-2  hidden sm:block">
                 <span className="text-cyan-500">03. </span>
-                <Link href="/">ABOUT</Link>
+                <Link className="text-white" href="/">ABOUT</Link>
                 </div>
 
                 <div className="block sm:hidden mr-10">
--- src/components/ResumeContainer.js
+++ src/components/ResumeContainer.js
@@ -16,12 +16,12 @@ export default function ResumeContainer() {
 
         <article className={`glass mt-[100px] max-h-[80vh] overflow-auto rounded shadow-xl z-30 flex flex-col  items-center  ${state ? "show" : "noshow"}  sm:m-10`}>
 
-            <h1 className='text-2xl lg:text-4xl font-extrabold mt-5'>RESUME</h1>
+            <h1 className='text-2xl lg:text-4xl font-extrabold mt-5 text-white'>RESUME</h1>
             <section className='flex flex-col lg:flex-row justify-center items-start'>
              
                 <div className=' m-10 lg:w-1/2'>
            
-                    <h1 className=' text-2xl lg:text-3xl font-bold mb-4'>Education</h1>
+                    <h1 className=' text-2xl lg:text-3xl font-bold mb-4 text-white'>Education</h1>
 
                        {resumeData.education.map((entry,idx)=>{
                         return   <ResumeEntries key={idx} data={entry} />
@@ -33,7 +33,7 @@ export default function ResumeContainer() {
 
                 <div className=' m-10 lg:w-1/2'>
 
-                <h1 className=' text-2xl lg:text-3xl font-bold mb-4'>Work Experience</h1>
+                <h1 className=' text-2xl lg:text-3xl font-bold mb-4 text-white'>Work Experience</h1>
 
                 {resumeData.workExp.map((workEntry,idx)=>{
                         return   <ResumeEntries key={idx} data={workEntry} />
